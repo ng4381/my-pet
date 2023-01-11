3e198786fd80b797cfcba371e948ebf84facc2a3
Nikita Goloveshko <nikitagoloveshko@gmail.com>
2023-01-11 17:44:48 +0200

    - 11/01/23

--- src/main/java/com/nservices/mypet/repository/LogRepository.java
+++ src/main/java/com/nservices/mypet/repository/LogRepository.java
@@ -3,7 +3,6 @@ package com.nservices.mypet.repository;
 import com.nservices.mypet.dto.ILogDto;
 import com.nservices.mypet.entity.LogEntity;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -13,7 +12,7 @@ import java.util.List;
 @Repository
 public interface LogRepository extends PagingAndSortingRepository<LogEntity, Long> {
 
-    @Query(value = "SELECT l.message as message, l.dateTime as dateTime FROM LogEntity l LEFT JOIN OwnerEntity o ON l.owner.id = o.id LEFT JOIN User u ON u.id = o.user.id WHERE u.username = :username ORDER BY l.id DESC")
+    @Query(value = "SELECT l.message as message, l.dateTime as dateTime FROM LogEntity l LEFT JOIN OwnerEntity o ON l.owner.id = o.id LEFT JOIN User u ON u.id = o.user.id WHERE u.username = :username ORDER BY l.dateTime DESC")
     List<ILogDto> findAllLogsByUsername(String username, Pageable pageable);
 
 }
--- src/test/java/com/nservices/mypet/repository/LogRepositoryTest.java
+++ src/test/java/com/nservices/mypet/repository/LogRepositoryTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
